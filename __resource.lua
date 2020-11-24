02f4b52e466e64bf4d1194f9abdc5c388c7d900b
Sokan <66193467+SokanPog@users.noreply.github.com>
2020-11-24 22:04:53 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+description 'Black Listed Weapons Script | Made By @Sokan#3333'
+
+author 'Sokan'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'config.lua',
+	'server/server.lua'	
+}
+
+client_scripts {
+	'config.lua',
+	'client/client.lua'
+}
---
+++ config.lua
@@ -0,0 +1,6 @@
+Config      = {
+    EnableKickPlayers = true,
+    BlackListedWeapons = { --Add Weapons by name and not by Hash. link: https://wiki.rage.mp/index.php?title=Weapons
+        "WEAPON_MINIGUN",
+    }
+}
